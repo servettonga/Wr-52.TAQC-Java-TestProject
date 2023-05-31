caa322ce7683baa8f7d96cc61d0a9912ee2e7c0a
Servet <serveth@gmail.com>
2023-05-31 10:55:00 +0200

    Refactor ConfigurationManager #6

--- src/test/java/net/eventsexpress/app/config/ConfigurationManager.java
+++ src/test/java/net/eventsexpress/app/config/ConfigurationManager.java
@@ -1,22 +1,21 @@
 package net.eventsexpress.app.config;
 
-import java.io.File;
+import java.net.URL;
 
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 
 public class ConfigurationManager {
-    protected static Configurations configs = new Configurations();
     protected static Configuration config;
 
     public static Configuration getConfig() {
         if (config == null) {
             try {
-                config = configs.properties(new File("src/test/resources/test.properties"));
+                URL resource = ConfigurationManager.class.getClassLoader().getResource("test.properties");
+                config = new Configurations().properties(resource);
             } catch (ConfigurationException e) {
                 System.out.println("Configuration file not found");
-                e.printStackTrace();
                 System.exit(1);
             }
         }
