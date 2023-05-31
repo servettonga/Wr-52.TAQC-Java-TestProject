c0c011c468510d78018f84713812f2466248f85a
Servet <83537967+servettonga@users.noreply.github.com>
2023-05-31 13:27:41 +0200

    Refactor TestSetUp (#19)

--- src/test/java/net/eventsexpress/app/tests/BaseTest.java
+++ src/test/java/net/eventsexpress/app/tests/BaseTest.java
@@ -8,18 +8,18 @@ import net.eventsexpress.app.config.ConfigurationManager;
 import net.eventsexpress.app.driver.DriverManager;
 import net.eventsexpress.app.utils.Utils;
 
-public class TestSetUp {
-    public static final Dotenv dotenv = Dotenv.load();
-    public static final String EMAIL = dotenv.get("EMAIL");
-    public static final String PASSWORD = dotenv.get("PASSWORD");
+abstract public class BaseTest {
+    public static final Dotenv DOTENV = Dotenv.load();
+    public static final String EMAIL = DOTENV.get("EMAIL");
+    public static final String PASSWORD = DOTENV.get("PASSWORD");
     public static final String NEW_EMAIL = Utils.randomUser().get(0);
     public static final String NEW_PASSWORD = Utils.randomUser().get(1);
-    public static final String baseURL = ConfigurationManager.getConfig().getString("baseURL");
+    public static final String BASE_URL = ConfigurationManager.getConfig().getString("BASE_URL");
 
     @BeforeMethod
     public void beforeMethod() {
         DriverManager.getDriver();
-        DriverManager.goToSite(baseURL);
+        DriverManager.goToSite(BASE_URL);
     }
 
     @AfterMethod
