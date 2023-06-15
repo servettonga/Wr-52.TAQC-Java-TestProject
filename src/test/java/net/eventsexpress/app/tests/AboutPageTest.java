bf6f112a4042d54cd25388b716411e2fdfa81e3b
kevin <kevin@LAPTOP-G9ARC70D>
2023-06-15 09:44:25 +0200

    Changed to Variable name to camel Case added @story and @Description annotations

--- src/test/java/net/eventsexpress/app/tests/AboutPageTest.java
+++ src/test/java/net/eventsexpress/app/tests/AboutPageTest.java
@@ -1,11 +1,18 @@
 package net.eventsexpress.app.tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import net.eventsexpress.app.pages.Footer;
 import net.eventsexpress.app.pages.footerpages.AboutPage;
 import org.testng.annotations.Test;
 
 public class AboutPageTest extends BaseTest{
-    @Test
+    @Test(priority = 3)
+    @Severity(SeverityLevel.NORMAL)
+    @Story("About Page Navigation")
+    @Description("Checks navigation to About Page and heading visibility")
     public void testUserIsRedirectedToAboutPage(){
         new Footer()
                 .navigateToAboutPage();
